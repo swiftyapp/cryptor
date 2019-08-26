76ae844fb4d86d0ca380f3bbd3812ccb5a4f0e84
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-08-26 12:58:40 +0200

    Basic implementation of aes-256-gcm algorithm

---
+++ index.js
@@ -0,0 +1,84 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Cryptor = Cryptor;
+
+var _crypto = _interopRequireDefault(require("crypto"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var ALGORITHM = 'aes-256-gcm';
+var SALT_LENGTH = 64;
+var IV_LENGTH = 16;
+var TAG_LENGTH = 16;
+
+function Cryptor(secret) {
+  if (!secret || typeof secret !== 'string') {
+    throw new Error('Cryptor: secret must be a non-empty string');
+  }
+
+  var hashedSecret = _crypto["default"].createHash('sha512').update(secret).digest('base64');
+
+  secret = null;
+
+  this.encrypt = function (value) {
+    checkValue(value);
+
+    var iv = _crypto["default"].randomBytes(IV_LENGTH);
+
+    var salt = _crypto["default"].randomBytes(SALT_LENGTH);
+
+    var key = generateKey(salt);
+
+    var cipher = _crypto["default"].createCipheriv(ALGORITHM, key, iv);
+
+    var encrypted = Buffer.concat([cipher.update(String(value), 'utf8'), cipher["final"]()]);
+    var tag = cipher.getAuthTag();
+    return Buffer.concat([salt, iv, tag, encrypted]).toString('hex');
+  };
+
+  this.decrypt = function (value) {
+    checkValue(value);
+    var stringValue = Buffer.from(String(value), 'hex');
+    var salt = getSalt(stringValue);
+    var iv = getIV(stringValue);
+    var tag = getTag(stringValue);
+    var encrypted = getEncryptedValue(stringValue);
+    var key = getKey(salt);
+
+    var decipher = _crypto["default"].createDecipheriv(ALGORITHM, key, iv);
+
+    decipher.setAuthTag(tag);
+    return decipher.update(encrypted, 'hex', 'utf8') + decipher["final"]('utf8');
+  };
+
+  var generateKey = function generateKey(salt) {
+    return _crypto["default"].pbkdf2Sync(hashedSecret, salt, 100000, 32, 'sha512');
+  };
+
+  var getSalt = function getSalt(string) {
+    return string.slice(0, SALT_LENGTH);
+  };
+
+  var getIV = function getIV(string) {
+    string.slice(SALT_LENGTH, SALT_LENGTH + IV_LENGTH);
+  };
+
+  var getTag = function getTag(string) {
+    string.slice(SALT_LENGTH + IV_LENGTH, SALT_LENGTH + IV_LENGTH + TAG_LENGTH);
+  };
+
+  var getEncryptedValue = function getEncryptedValue(string) {
+    string.slice(SALT_LENGTH + IV_LENGTH + 16);
+  };
+
+  var checkValue = function checkValue(value) {
+    if (!value) {
+      throw new Error('Cryptor: value must not be null or undefined');
+    }
+
+    return true;
+  };
+}
---
+++ test/index.js
@@ -0,0 +1,73 @@
+import test from 'tape'
+import { Cryptor } from '../'
+
+const secret = 'mysecretpassword'
+const data = 'sensitive data to encrypt'
+
+test('Encrypts and decrypts data', t => {
+  t.plan(1)
+
+  const cryptor = new Cryptor(secret)
+  const encrypted = cryptor.encrypt(data)
+  const decrypted = cryptor.decrypt(encrypted)
+
+  t.equal(decrypted, data)
+})
+
+test('Encryptor generates a different value each time', t => {
+  t.plan(1)
+
+  const cryptor = new Cryptor(secret)
+  const encrypted1 = cryptor.encrypt(data)
+  const encrypted2 = cryptor.encrypt(data)
+
+  t.notEqual(encrypted1, encrypted2)
+})
+
+test('Throws an error if no secret is set for Cryptor', t => {
+  t.plan(5)
+  t.throws(() => new Cryptor(), 'Cryptor: secret must be a non-empty string')
+  t.throws(
+    () => new Cryptor(null),
+    'Cryptor: secret must be a non-empty string'
+  )
+  t.throws(() => new Cryptor(0), 'Cryptor: secret must be a non-empty string')
+
+  t.throws(
+    () => new Cryptor(Integer(12345678)),
+    'Cryptor: secret must be a non-empty string'
+  )
+
+  t.throws(
+    () => new Cryptor(Buffer.from('12345678')),
+    'Cryptor: secret must be a non-empty string'
+  )
+})
+
+test('Throws an error if encryption value is null or undefined', t => {
+  t.plan(2)
+
+  const cryptor = new Cryptor(secret)
+  t.throws(
+    () => cryptor.encrypt(),
+    'Cryptor: secret must be a non-empty string'
+  )
+  t.throws(
+    () => cryptor.encrypt(null),
+    'Cryptor: secret must be a non-empty string'
+  )
+})
+
+test('Throws an error if decryption value is null or undefined', t => {
+  t.plan(2)
+
+  const cryptor = new Cryptor(secret)
+  t.throws(
+    () => cryptor.decrypt(),
+    'Cryptor: secret must be a non-empty string'
+  )
+  t.throws(
+    () => cryptor.decrypt(null),
+    'Cryptor: secret must be a non-empty string'
+  )
+})
